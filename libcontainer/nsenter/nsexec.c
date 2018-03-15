4930a61498b8092bd0dabbb7e282f77abbb7b4fb
Jonathan Boulle <jonathanboulle@gmail.com>
2018-03-15 14:10:49 +0100

    nsexec: ignore permission errors on oom_score_adj
    If we're running in a context in which we don't have permission to
    set oom_score_adj (for example, if it's mounted read-only, which is the
    case in some container contexts), we should just continue anyway.
    
    Ref: https://github.com/lxc/lxd/issues/2994#issuecomment-283759615

--- libcontainer/nsenter/nsexec.c
+++ libcontainer/nsenter/nsexec.c
@@ -303,8 +303,10 @@ static void update_oom_score_adj(char *data, size_t len)
 	if (data == NULL || len <= 0)
 		return;
 
-	if (write_file(data, len, "/proc/self/oom_score_adj") < 0)
-		bail("failed to update /proc/self/oom_score_adj");
+	if (write_file(data, len, "/proc/self/oom_score_adj") < 0) {
+		if (errno != EPERM && errno != EACCES)
+			bail("failed to update /proc/self/oom_score_adj");
+	}
 }
 
 /* A dummy function that just jumps to the given jumpval. */
